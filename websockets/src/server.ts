0da94366f52692fb1013bdc07e4470d598960980
Anil Kumar <akryptic00@gmail.com>
2025-04-11 02:49:35 +0530

    fix: decrease alive time to detect dead connections more faster.

--- websockets/src/server.ts
+++ websockets/src/server.ts
@@ -1,12 +1,14 @@
 import WebSocket, { WebSocketServer } from "ws";
-import { handleMessage } from "./messageHandler";
 import { broadcastToWebApps, removeClient } from "./clients";
 import { setupHeartbeat, startHeartbeat } from "./heartbeat";
+import { handleMessage } from "./messageHandler";
 
 export function createWebSocketServer(port: number) {
   const wss = new WebSocketServer({ port });
 
   wss.on("connection", (ws: WebSocket) => {
+    //@ts-ignore
+    ws._socket.setKeepAlive(true, 8000);
     const roleRef = { role: null as string | null };
 
     setupHeartbeat(ws);
